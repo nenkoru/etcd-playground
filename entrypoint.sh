52b4b69fd8478b8d10b8624b5bb001eac7a82170
Chernenko Ruslan <ractyfree@gmail.com>
2022-12-11 17:00:29 +0300

    Add external docker network to connect to discovery service

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@ etcd \
 --initial-advertise-peer-urls http://$HOSTNAME:2380 \
 --listen-peer-urls http://0.0.0.0:2380 \
 --listen-client-urls http://0.0.0.0:2379 \
---advertise-client-urls http://0.0.0.0:2379 
+--advertise-client-urls http://$HOSTNAME:2379 
+
