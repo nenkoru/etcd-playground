bbb8ad78ff3b5cd1bdee00943733712d26440597
Chernenko Ruslan <ractyfree@gmail.com>
2022-12-11 15:23:53 +0300

    Change from pre-determined cluster info to dynamic etcd service discovery

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 etcd \
---name $_ETCD_NAME \
---initial-cluster-state new \
---initial-cluster $_ETCD_CLUSTER \
---initial-cluster-token etcd-cluster-1 \
---initial-advertise-peer-urls http://$_ETCD_NAME:2380 \
+--name $HOSTNAME \
+--initial-advertise-peer-urls http://$HOSTNAME:2380 \
 --listen-peer-urls http://0.0.0.0:2380 \
 --listen-client-urls http://0.0.0.0:2379 \
---advertise-client-urls http://0.0.0.0:2379
+--advertise-client-urls http://0.0.0.0:2379 
